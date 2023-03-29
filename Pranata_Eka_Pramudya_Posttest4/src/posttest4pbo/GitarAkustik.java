01a16bdff082190f27f1bf37565f5a233395a614
pramudyaeka <113302725+pramudyaeka@users.noreply.github.com>
2023-03-29 23:57:50 +0800

    Add files via upload

---
+++ Pranata_Eka_Pramudya_Posttest4/src/posttest4pbo/GitarAkustik.java
@@ -0,0 +1,38 @@
+package posttest4pbo;
+public class GitarAkustik extends Gitar 
+{
+    private String kode;
+    protected String jenis_kayu;
+    
+    public GitarAkustik (String nama, int stok, int harga, String kode, String jenis_kayu)
+    {
+        super(nama,stok, harga);
+        this.kode = kode;
+        this.jenis_kayu = jenis_kayu;
+    }
+
+    public String getJenis_kayu() {
+        return jenis_kayu;
+    }
+
+    public void setJenis_kayu(String jenis_kayu) {
+        this.jenis_kayu = jenis_kayu;
+    }
+
+    public String getKode() {
+        return kode;
+    }
+
+    public void setKode(String kode) {
+        this.kode = kode;
+    }
+    
+//    Override
+    @Override
+    public void display()
+    {
+        super.display();
+        System.out.println(">>>> || Jenis Kayu      : "+ jenis_kayu);
+        System.out.println("=============================================");
+    }
+}
